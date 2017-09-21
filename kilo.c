a468494b452d9b5079526378acd81a5ef65b5534
jmks <jason.schweier@gmail.com>
2017-09-20 23:45:22 -0400

    read from stdin in raw mode; reset terminal after running!

--- kilo.c
+++ kilo.c
@@ -1,7 +1,28 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+  struct termios raw;
+
+  // read terminal attributes
+  tcgetattr(STDIN_FILENO, &raw);
+
+  // lflags is local flags
+  // other flags include input flags (c_iflag), output flags (c_oflag) and
+  // control flags (c_cflag)
+  raw.c_lflag &= ~(ECHO);
+
+  // write terminal attributes
+  // TCSAFLUSH waits for all output to be written to terminal before applying change
+  // also discards anything not yet read
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+  enableRawMode();
+
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+
   return 0;
 }
