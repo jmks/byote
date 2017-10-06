08299422c4dd0ccb80df5780aec128c32a2bfb08
jmks <jason.schweier@gmail.com>
2017-10-06 18:52:45 -0400

    able to delete a row. BACKSPACE at the beginning of row moves text to previous row

--- kilo.c
+++ kilo.c
@@ -393,6 +393,17 @@ void editorRowInsertChar(erow *row, int at, int c) {
   E.dirty++;
 }
 
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+
+  row->size += len;
+  row->chars[row->size] = '\0';
+
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -408,6 +419,19 @@ void editorAppendRow(char *s, size_t len) {
   editorUpdateRow(&E.row[at]);
 
   E.numrows++;
+  E.dirty++;
+}
+
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
   E.dirty++;
 }
 
@@ -436,11 +460,20 @@ void editorInsertChar(int c) {
 
 void editorDelChar() {
   if (E.cy == E.numrows) return;
+  // at beginning of file, so nothing to delete
+  if (E.cx == 0 && E.cy == 0) return;
 
   erow *row = &E.row[E.cy];
   if (E.cx > 0) {
     editorRowDelChar(row, E.cx - 1);
     E.cx--;
+  } else {
+    // cursor at beginning of row
+    // move row's text to previous row
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
   }
 }
 
