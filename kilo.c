80b9d3f9d79e9afde1630e861c4d1a5fae6dcd92
jmks <jason.schweier@gmail.com>
2017-10-06 19:18:57 -0400

    allow user to input filename when saving to disk with no given file

--- kilo.c
+++ kilo.c
@@ -71,6 +71,7 @@ void editorDelChar();
 void editorInsertChar();
 void editorSave();
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen();
 
 /*** terminal ***/
 
@@ -186,6 +187,35 @@ int editorReadKey() {
 }
 
 /*** input ***/
+
+char *editorPrompt(char *prompt) {
+  size_t bufsize = 128;
+  char *buf = malloc(bufsize);
+
+  size_t buflen = 0;
+  buf[0] = '\0';
+
+  while (1) {
+    editorSetStatusMessage(prompt, buf);
+    editorRefreshScreen();
+
+    int c = editorReadKey();
+    if (c == '\r') {
+      if (buflen != 0) {
+        editorSetStatusMessage("");
+        return buf;
+      }
+    } else if (!iscntrl(c) && c < 128) {
+      if (buflen == bufsize - 1) {
+        bufsize *= 2;
+        buf = realloc(buf, bufsize);
+      }
+
+      buf[buflen++] = c;
+      buf[buflen] = '\0';
+    }
+  }
+}
 
 void editorMoveCursor(int key) {
   erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
@@ -547,7 +577,9 @@ void editorOpen(char *filename) {
 }
 
 void editorSave() {
-  if (E.filename == NULL) return;
+  if (E.filename == NULL) {
+    E.filename = editorPrompt("Save as: %s");
+  }
 
   int len;
   char *buf = editorRowsToString(&len);
